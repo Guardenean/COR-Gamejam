7d9bfe30a4c20035a4b8e318844171fc1666e87b
Guardenean <143855121+Guardenean@users.noreply.github.com>
2024-11-28 10:10:34 -0300

    Commit Base
    - Menu inicial simples

---
+++ Scripts/inicio.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_jogar_pressed() -> void: #Entra no jojinho
+	get_tree().change_scene_to_file("res://Cenas/Principal.tscn")
+
+func _on_sair_pressed() -> void:  #Sai do jojinho
+	get_tree().quit()
